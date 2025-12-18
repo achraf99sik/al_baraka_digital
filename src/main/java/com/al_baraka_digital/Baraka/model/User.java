30c13da59d3db80d4008e12b4758f6b9ec54aff7
achraf <achrafsikal@gmail.com>
2025-12-18 12:15:13 +0100

    refactor: Extend User model to implement UserDetails interface for Spring Security integration, adding methods for user authentication and role management.

--- src/main/java/com/al_baraka_digital/Baraka/model/User.java
+++ src/main/java/com/al_baraka_digital/Baraka/model/User.java
@@ -3,14 +3,19 @@ package com.al_baraka_digital.Baraka.model;
 import com.al_baraka_digital.Baraka.enums.Role;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Data
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue
@@ -42,5 +47,35 @@ public class User {
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
     }
 }
