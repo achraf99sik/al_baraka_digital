101d1145b529bb281a42510ab3fbc08449d819a6
achraf <achrafsikal@gmail.com>
2025-12-19 13:30:45 +0100

    feat: Define custom business exceptions for granular error handling strategies.

---
+++ src/main/java/com/al_baraka_digital/Baraka/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.al_baraka_digital.Baraka.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
