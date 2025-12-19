b7e2798f29f0e00ee0ddeab061a1450c7f061e88
achraf <achrafsikal@gmail.com>
2025-12-19 11:20:30 +0100

    feat: Create JPA repositories for User and Account management with UUID support.

--- src/main/java/com/al_baraka_digital/Baraka/repository/AccountRepository.java
+++ src/main/java/com/al_baraka_digital/Baraka/repository/AccountRepository.java
@@ -1,14 +1,16 @@
 package com.al_baraka_digital.Baraka.repository;
 
+import com.al_baraka_digital.Baraka.model.Account;
 import com.al_baraka_digital.Baraka.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByEmail(String email);
-    boolean existsByEmail(String email);
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+    Optional<Account> findByAccountNumber(String accountNumber);
+    Optional<Account> findByOwner(User owner);
 }
