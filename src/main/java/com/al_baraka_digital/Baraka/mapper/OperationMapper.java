bef98f44dd5e077bdcb9f80e3ab9ffdace36830a
achraf <achrafsikal@gmail.com>
2025-12-22 13:45:25 +0100

    feat: Implement MapStruct mappers to convert between Entities and DTOs efficiently.

--- src/main/java/com/al_baraka_digital/Baraka/mapper/OperationMapper.java
+++ src/main/java/com/al_baraka_digital/Baraka/mapper/OperationMapper.java
@@ -1,7 +1,6 @@
 package com.al_baraka_digital.Baraka.mapper;
 
-import com.al_baraka_digital.Baraka.dto.CreateOperationRequestDTO;
-import com.al_baraka_digital.Baraka.dto.OperationDTO;
+import com.al_baraka_digital.Baraka.dto.OperationResponse;
 import com.al_baraka_digital.Baraka.model.Operation;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -9,17 +8,7 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface OperationMapper {
 
-    @Mapping(source = "accountSource.id", target = "accountSourceId")
-    @Mapping(source = "accountDestination.id", target = "accountDestinationId")
-    OperationDTO toDTO(Operation operation);
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "status", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "validatedAt", ignore = true)
-    @Mapping(target = "executedAt", ignore = true)
-    @Mapping(target = "accountSource", ignore = true)
-    @Mapping(target = "accountDestination", ignore = true)
-    @Mapping(target = "document", ignore = true)
-    Operation fromCreateRequest(CreateOperationRequestDTO dto);
+    @Mapping(source = "accountSource.accountNumber", target = "sourceAccountNumber")
+    @Mapping(source = "accountDestination.accountNumber", target = "destinationAccountNumber")
+    OperationResponse toResponse(Operation operation);
 }
