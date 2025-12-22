4b026669a8859f24cfffad508bde160ed90e916a
achraf <achrafsikal@gmail.com>
2025-12-22 10:45:33 +0100

    refactor: Extract AccountService interface to decouple business logic definition from implementation.

---
+++ src/main/java/com/al_baraka_digital/Baraka/service/AccountService.java
@@ -0,0 +1,14 @@
+package com.al_baraka_digital.Baraka.service;
+
+import com.al_baraka_digital.Baraka.model.Account;
+import com.al_baraka_digital.Baraka.model.User;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+public interface AccountService {
+    Account createAccount(User user);
+    Account getAccountByUser(User user);
+    Account getAccountByNumber(String accountNumber);
+    void updateBalance(Account account, BigDecimal amount);
+}
