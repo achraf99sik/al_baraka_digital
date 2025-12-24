0f432b38374b86b95be10921e9f4e83b1a9199a0
achraf <achrafsikal@gmail.com>
2025-12-24 17:10:42 +0100

    chore: Remove deprecated FileStorageService and duplicate JwtAuthenticationFilter.

--- src/main/java/com/al_baraka_digital/Baraka/service/FileStorageService.java
+++
@@ -1,8 +0,0 @@
-package com.al_baraka_digital.Baraka.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileStorageService {
-
-    String store(MultipartFile file);
-}
