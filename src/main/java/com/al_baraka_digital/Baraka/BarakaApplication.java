3bdecbab96245dbe8841c83c9a956452f6911c3e
achraf <achrafsikal@gmail.com>
2025-12-26 10:50:00 +0100

    chore: Clean up main application class.

--- src/main/java/com/al_baraka_digital/Baraka/BarakaApplication.java
+++ src/main/java/com/al_baraka_digital/Baraka/BarakaApplication.java
@@ -8,19 +8,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BarakaApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
+		String ssl = "?sslmode=require&channel_binding=require";
+		ssl = "";
 		String postgresUri = String.format(
-				"jdbc:postgresql://%s:%s/%s?sslmode=require&channel_binding=require",
-				dotenv.get("PG_HOST"),
-				dotenv.get("PG_PORT"),
-				dotenv.get("PG_DB")
+				"jdbc:postgresql://%s:%s/%s%s",
+				require("PG_HOST"),
+				require("PG_PORT"),
+				require("PG_DB"),
+				ssl
 		);
 
 		System.setProperty("spring.datasource.url", postgresUri);
-		System.setProperty("spring.datasource.username", dotenv.get("PG_USERNAME"));
-		System.setProperty("spring.datasource.password", dotenv.get("PG_PASSWORD"));
+		System.setProperty("spring.datasource.username", require("PG_USERNAME"));
+		System.setProperty("spring.datasource.password", require("PG_PASSWORD"));
 		SpringApplication.run(BarakaApplication.class, args);
 	}
+	private static String require(String key) {
+		Dotenv env = Dotenv.configure().ignoreIfMissing().load();
+		String value = env.get(key);
+		if (value == null) {
+			throw new IllegalStateException("Missing env variable: " + key);
+		}
+		return value;
+	}
+
 
 }
